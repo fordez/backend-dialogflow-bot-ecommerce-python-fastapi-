2981fb4981a5fe5b6443a80e486b867d32fa9039
Fernando <fercho_blk@hotmail.com>
2021-10-28 11:06:00 -0500

    response module final

--- api/modelResponse.py
+++ api/modelResponse.py
@@ -1,6 +1,60 @@
-from typing import Optional, Dict, Any
-from pydantic import BaseModel
+class Fulfillment:
+    text: str
+
+    def __init__(self, text):
+        self.text = text
+
+    def tex(self) -> dict:
+        return {"fulfillmentText": self.text}
 
 
-class Fulfillment(BaseModel):
-    fulfillmentText: Dict[str, Any]
+class Card:
+    title: str
+    subtitle: str
+    imageUri: str
+    buttonText1: str
+    postback1: str
+    buttonText2: str
+    postback2: str
+    buttonText3: str
+    postback3: str
+
+    def __init__(self, title, subtitle, imageUri,
+                 buttonText1, postback1,
+                 buttonText2, postback2,
+                 buttonText3, postback3):
+
+        self.title = title
+        self.subtitle = subtitle
+        self.imageUri = imageUri
+        self.buttonText1 = buttonText1
+        self.postback1 = postback1
+        self.buttonText2 = buttonText2
+        self.postback2 = postback2
+        self.buttonText3 = buttonText3
+        self.postback3 = postback3
+
+    def resposeCard(self) -> dict:
+        return {"fulfillmentMessages": [
+            {
+                "card": {
+                    "title": self.title,
+                    "subtitle": self.subtitle,
+                    "imageUri": self.imageUri,
+                    "buttons": [
+                        {
+                            "text": self.buttonText1,
+                            "postback": self.postback1
+                        },
+                        {
+                            "text": self.buttonText2,
+                            "postback": self.postback2
+                        },
+                        {
+                            "text": self.buttonText3,
+                            "postback": self.postback3
+                        }
+                    ]
+                }
+            }
+        ]}
--- api/response.py
+++ api/response.py
@@ -1,39 +1,41 @@
-from modelResponse import Fulfillment
-
-fulfillmentMessages = {"fulfillmentMessages": [
-    {
-        "card": {
-            "title": "Fordez",
-            "subtitle": "catalogo de productos para la venta",
-            "imageUri": "https://i.imgur.com/tzWhjX3.png",
-            "buttons": [
-                {
-                        "text": "Agregar al carrito",
-                        "postback": "carrito"
-                },
-                {
-                    "text": "Mas detalle",
-                    "postback": "detalle"
-                },
-                {
-                    "text": "Comprar",
-                    "postback": "comprar"
-                }
-            ]
-        }
-    }
-
-]}
+from typing import Text
+from modelResponse import Fulfillment, Card
 
 
-def saludo(saludo: Fulfillment) -> dict:
-    Saludo = saludo
-    return Saludo
+def fulfillment(text) -> dict:
+    fulfillmenText = Fulfillment(text)
+    return fulfillmenText.tex()
+
+
+def card(title, subtitle, imageUri,
+         buttonText1, postback1,
+         buttonText2, postback2,
+         buttonText3, postback3) -> dict:
+
+    card = Card(title, subtitle, imageUri,
+                buttonText1, postback1,
+                buttonText2, postback2,
+                buttonText3, postback3)
+
+    return card.resposeCard()
 
 
 def response(dataOrigin, dataNlp,) -> dict:
-    if (dataOrigin['source'] == "facebook"):
-        response = saludo('hoa lucy tumaco')
+    if (dataOrigin['source'] != "facebook"):
+        response = fulfillment("Hola madre")
     else:
-        response = fulfillmentMessages
+        title = "Fordez"
+        subtitle = "catalogo de productos para la venta"
+        imageUri = "https://i.imgur.com/tzWhjX3.png"
+        buttonText1 = "Agregar al carrito"
+        postback1 = "carrito"
+        buttonText2 = "Mas detalle"
+        postback2 = "detalle"
+        buttonText3 = "Comprar"
+        postback3 = "comprar"
+
+        response = card(title, subtitle, imageUri,
+                        buttonText1, postback1,
+                        buttonText2, postback2,
+                        buttonText3, postback3)
     return response
